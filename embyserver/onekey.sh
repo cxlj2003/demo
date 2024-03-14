47a5b4d4dd015a49fe05de969a79533248a09ba2
cxlj <chuxiaojun2003@qq.com>
2024-03-14 17:09:17 +0800

    Update onekey.sh

--- embyserver/onekey.sh
+++ embyserver/onekey.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt -y  install docker.io docker-compose
-mkdir -p /opt/sh/embyserver
-cd /opt/sh/embyserver
-curl -o compose.yml https://raw.githubusercontent.com/cxlj2003/demo/main/embyserver/compose.yml
+mkdir -p /opt/sh/embyserver && cd /opt/sh/embyserver
+if [ -!f /opt/sh/embyserver/compose.yml ]; then
+	curl --connect-timeout 2 -o compose.yml https://raw.githubusercontent.com/cxlj2003/demo/main/embyserver/compose.yml
+	if [ -!f /opt/sh/embyserver/compose.yml ]; then
+		curl --connect-timeout 2 -o compose.yml https://gitee.com/cxlj2003/demo/raw/main/embyserver/compose.yml
+	fi
+fi
 docker-compose up -d
