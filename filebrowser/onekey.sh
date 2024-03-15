2643180202316acf787c95761f2954a404b48459
cxlj <chuxiaojun2003@qq.com>
2024-03-15 16:38:28 +0800

    update

--- filebrowser/onekey.sh
+++ filebrowser/onekey.sh
@@ -2,7 +2,7 @@
 set -x
 export DEBIAN_FRONTEND=noninteractive
 apt -y -qq install docker.io docker-compose
-prj=seafile
+prj=filebrowser
 mkdir -p /opt/sh/${prj} && cd /opt/sh/${prj}
 curl https://raw.githubusercontent.com/cxlj2003/demo/main/${prj}/compose.yml --connect-timeout 2 -o compose.yml
 if [ $? != 0 ]; then
