4d7e778b3ed0caa05b6955a5005ce979746b4a7a
cxlj <chuxiaojun2003@qq.com>
2024-09-13 17:20:43 +0800

    Update macvlan.sh

--- DSM/macvlan.sh
+++ DSM/macvlan.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-cat << EOF >>/etc/rc.local
-ip link add vdsm-shim link ens32 type macvlan  mode bridge
-ip addr add 198.19.201.121/24 dev vdsm-shim
-ip link set vdsm-shim up
-ip route add 198.19.201.120/32 dev vdsm-shim
+cat >/etc/networkd-dispatcher/routable.d/macvlan-interfaces.sh<<EOF
+#! /bin/bash
+ip link add vdsm-shim link ens32 type macvlan mode bridge
 EOF
-chmod +x /etc/rc.local
-systemctl enable rc-local --now
+chmod +x /etc/networkd-dispatcher/routable.d/macvlan-interfaces.sh
+
+cat <<EOF >/etc/netplan/macvlan-interfaces.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    vdsm-shim:
+      addresses:
+        - 198.19.201.121/24
+      routes:
+        - to: 198.19.201.120/32
+          via: 198.19.201.121
+EOF
+chmod 600 /etc/netplan/macvlan-interfaces.yaml
