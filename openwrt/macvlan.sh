1e7debb3d89b7378f748801ad61981658b97370a
cxlj <chuxiaojun2003@qq.com>
2024-09-14 17:46:23 +0800

    Update macvlan.sh

--- openwrt/macvlan.sh
+++ openwrt/macvlan.sh
@@ -18,3 +18,10 @@ network:
           via: 198.19.255.121
 EOF
 chmod 600 /etc/netplan/macvlan-interfaces.yaml
+
+
+####openwrt 
+####immortalwrt https://github.com/immortalwrt/immortalwrt
+####lean 
+####istoreos 
+####kwrt 10.0.0.1 密码root  https://github.com/kiddin9/Kwrt https://openwrt.ai/docker%E7%89%88openwrt%E6%97%81%E8%B7%AF%E7%94%B1%E5%AE%89%E8%A3%85%E8%AE%BE%E7%BD%AE%E6%95%99%E7%A8%8B/
