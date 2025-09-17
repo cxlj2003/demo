fbdf7528621d5b8e44c4039a0627fc968f7d24f9
cxlj <chuxiaojun2003@qq.com>
2025-09-17 13:13:38 +0800

    update

--- local_mirrors/start.sh
+++ local_mirrors/start.sh
@@ -10,17 +10,26 @@ update_downloader(){
 local reponame=$1
 local mirrors_root=/opt/mirrors
 local repo_root=${mirrors_root}/${reponame}
-local repo_list=$(yum repolist |grep -E ${reponame}  |awk  '{print $1}' |xargs)
+local repo_list=$(yum repolist enabled |grep -E ${reponame}  |awk  '{print $1}' |xargs)
 
 for repo in ${repo_list};do 
-  if [ ! -d ${repo_root}/${repo} ];then
+  if [[ ! -d ${repo_root}/${repo} ]];then
     mkdir -p ${repo_root}/${repo}/Packages/
   fi
-  if [ ! -d ${repo_root}/${repo}/comps.xml ];then 
-   reposync --repoid ${repo} -p ${repo_root} --downloadcomps
-   createrepo --groupfile ${repo_root}/${repo}/comps.xml ${repo_root}
+  ##同步文件
+  reposync --repoid ${repo} -np  ${repo_root} --downloadcomps --delete
+  ##生成元数据
+  if [[ ! -f ${repo_root}/${repo}/repodata/repomd.xml ]];then
+    echo "初次生成元数据" 
+    if [[ ! -f ${repo_root}/${repo}/comps.xml ]];then 
+      echo "没有找到 ${repo_root}/${repo}/comps.xml 文件，跳过生成组元数据"
+      createrepo --repo ${repo}  ${repo_root}
+    else
+      echo "找到 ${repo_root}/${repo}/comps.xml 文件，生成组元数据"
+      createrepo --repo ${repo} --groupfile ${repo_root}/${repo}/comps.xml ${repo_root}
+    fi
   else
-    reposync --repoid ${repo} -np  ${repo_root} --downloadcomps
+    echo "增量更新元数据"
     createrepo --repo ${repo} --update  ${repo_root}/${repo}
   fi
 done 
