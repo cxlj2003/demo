ae8313265f7eb1d51853c422ea3aee3b2770e90c
cxlj <chuxiaojun2003@qq.com>
2025-09-19 09:57:33 +0800

    Update start.sh
    update

--- local_mirrors/start.sh
+++ local_mirrors/start.sh
@@ -2,6 +2,11 @@
 set -ex
 
 ##创建增量更新脚本
+# mkdir -p /data/repo/V10-SP3-x86
+# reposync -m -p /data/repo/V10-SP3-x86/
+# cd /data/repo/V10-SP3-x86/
+# createrepo -g ks10-adv-os/comps.xml .
+
 
 cat << 'EOF' > /root/update_mirrors.sh
 #!/bin/bash
@@ -13,35 +18,36 @@ local repo_root=${mirrors_root}/${reponame}
 local repo_list=$(yum repolist enabled |grep -E ${reponame}  |awk  '{print $1}' |xargs)
 
 for repo in ${repo_list};do 
-  if [[ ! -d ${repo_root}/${repo} ]];then
-    mkdir -p ${repo_root}/${repo}/Packages/
-  fi
+
   ##同步文件
   reposync --repoid ${repo} -np  ${repo_root} --downloadcomps --delete
+  local comps_file=${repo_root}/${repo}/comps.xml
+  local repond_file=${repo_root}/${repo}/repodata/repomd.xml
   ##生成元数据
-  if [[ ! -f ${repo_root}/${repo}/repodata/repomd.xml ]];then
+  if [[ ! -f ${repond_file} ]];then
     echo "初次生成元数据" 
-    if [[ ! -f ${repo_root}/${repo}/comps.xml ]];then 
-      echo "没有找到 ${repo_root}/${repo}/comps.xml 文件，跳过生成组元数据"
-      createrepo --repo ${repo}  ${repo_root}/${repo}
+    if [[ ! -f ${comps_file} ]];then 
+      echo "没有找到 ${comps_file} 文件，跳过生成组元数据"
+      createrepo_c -p --repo ${repo}  ${repo_root}/${repo} --workers 10
     else
-      echo "找到 ${repo_root}/${repo}/comps.xml 文件，生成组元数据"
-      createrepo --repo ${repo} --groupfile ${repo_root}/${repo}/comps.xml ${repo_root}/${repo}
+      echo "找到 ${comps_file} 文件，生成组元数据"
+      createrepo_c -p --repo ${repo} --groupfile ${comps_file} ${repo_root}/${repo} --workers 10
     fi
   else
     echo "增量更新元数据"    
-    if [[ ! -f ${repo_root}/${repo}/comps.xml ]];then 
-      echo "没有找到 ${repo_root}/${repo}/comps.xml 文件，跳过生成组元数据"
-      createrepo --repo ${repo} --update  ${repo_root}/${repo}
+    if [[ ! -f ${comps_file} ]];then 
+      echo "没有找到 ${comps_file} 文件，跳过生成组元数据"
+      createrepo_c -p --repo ${repo} --update  ${repo_root}/${repo} --workers 10
     else
-      echo "找到 ${repo_root}/${repo}/comps.xml 文件，生成组元数据"
-      createrepo --repo ${repo} --groupfile ${repo_root}/${repo}/comps.xml --update  ${repo_root}/${repo}
+      echo "找到 $comps_file} 文件，生成组元数据"
+      createrepo_c -p  --repo ${repo} --groupfile ${comps_file} --update  ${repo_root}/${repo} --workers 10
     fi
   fi
 done 
 }
+
 update_downloader kylin &> /dev/null &
-update_downloader inlinux &> /dev/null &
+#update_downloader inlinux &> /dev/null &
 set +ex
 EOF
 
